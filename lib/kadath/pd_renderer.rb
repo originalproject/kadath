f7d4f0c752c38b2171e448bbf4c6223e6c464eb6
Matthew Simpson <matthewsimpson@gmail.com>
2013-10-20 16:29:03 -0700

    Start renderer with box rendering method & add private method test helpers

--- lib/kadath/pd_renderer.rb
+++ lib/kadath/pd_renderer.rb
@@ -1,4 +1,31 @@
 module Kadath
   class PdRenderer
+
+    private
+
+    Y_SPACING = 32
+
+    def yield_object(pd_box)
+      return if box_registered?(pd_box)
+      index = register_box(pd_box)
+      yield "obj 0 #{ index * Y_SPACING } #{ pd_box.pd_object }"
+    end
+
+    def box_registered?(pd_box)
+      registry.has_key?(pd_box.object_id)
+    end
+
+    def register_box(pd_box)
+      registry[pd_box.object_id] = take_registry_index!
+    end
+
+    def registry
+      @registry ||= {}
+    end
+
+    def take_registry_index!
+      @registry_index = (@registry_index || -1) + 1
+    end
+
   end
 end
--- spec/pd_renderer_spec.rb
+++ spec/pd_renderer_spec.rb
@@ -3,7 +3,44 @@ require_relative 'spec_helper'
 
 describe PdRenderer do
 
-  it "can render a network"
+  before do
+    publicize_class(PdRenderer)
+  end
+
+  after do
+    unpublicize_class(PdRenderer)
+  end
+
+  it "can render a pd object to a block" do
+    box = mock(pd_object: "foo")
+    pdr = PdRenderer.new
+    output = nil
+    pdr.yield_object(box) { |s| output = s }
+    output.must_equal "obj 0 0 foo"
+  end
+
+  it "renders each pd object at a different y offset" do
+    box1 = mock(pd_object: "foo")
+    box2 = mock(pd_object: "bar")
+    pdr = PdRenderer.new
+    output = nil
+    pdr.yield_object(box1) { }
+    pdr.yield_object(box2) { |s| output = s }
+    output.must_equal "obj 0 32 bar"
+  end
+
+  it "will not render the same pd object twice" do
+    box = stub(pd_object: "foo")
+    pdr = PdRenderer.new
+    output = 'never changed'
+    pdr.yield_object(box) { }
+    pdr.yield_object(box) { |s| output = s }
+    output.must_equal 'never changed'
+  end
+
+  it "can render the boxes in a network, one at a time, to a block"
+
+  it "can render a network, one object or connection at a time, to a block"
 
   # it "can render a pd box" do
   #   pdr = PdRenderer.new
