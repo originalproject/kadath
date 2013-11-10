f8dbe8081892be3be511803486c3c31f4c49c3e4
Matthew Simpson <matthewsimpson@gmail.com>
2013-11-10 15:48:29 -0800

    Add audio functions to JRPD connector

--- lib/kadath/jrpd_connector.rb
+++ lib/kadath/jrpd_connector.rb
@@ -3,13 +3,31 @@ require 'jrpd'
 module Kadath
   class JRPDConnector
 
-    def initialize
-      pd_file_path = File.join(Kadath.gem_root, 'data/kadath.pd')
-      @patch = JRPD::Patch.new(pd_file_path)
+    def send_to_patch(unstructured_msg)
+      patch.send_unstructured_message(unstructured_msg)
     end
 
-    def send_to_patch(unstructured_msg)
-      @patch.send_unstructured_message(unstructured_msg)
+    def start_audio
+      audio.start
+    end
+
+    def stop_audio
+      audio.stop
+    end
+
+    private
+
+    def patch
+      @patch ||= open_patch
+    end
+
+    def open_patch
+      pd_file_path = File.join(Kadath.gem_root, 'data/kadath.pd')
+      JRPD::Patch.new(pd_file_path)
+    end
+
+    def audio
+      @audio ||= JRPD::Audio.new
     end
 
   end
--- spec/unit/jrpd_connector_spec.rb
+++ spec/unit/jrpd_connector_spec.rb
@@ -7,9 +7,12 @@ describe JRPDConnector do
     Kadath.stubs(:gem_root).returns('root')
   end
 
-  it "tells JRPD to open the kadath pd file when initialized" do
-    JRPD::Patch.expects(:new).with('root/data/kadath.pd')
+  it "uses JRPD to open the kadath pd file" do
+    publicize_class(JRPDConnector)
+    JRPD::Patch.expects(:new).with('root/data/kadath.pd').returns('foo')
     jc = JRPDConnector.new
+    jc.patch.must_equal 'foo'
+    unpublicize_class(JRPDConnector)
   end
 
   it "sends arbitrary messages to the patch" do
@@ -17,7 +20,23 @@ describe JRPDConnector do
     mock_patch.expects(:send_unstructured_message).with('foo')
     JRPD::Patch.stubs(:new).with('root/data/kadath.pd').returns(mock_patch)
     jc = JRPDConnector.new
-    jc.send_to_patch('foo')    
+    jc.send_to_patch('foo')
+  end
+
+  it "starts Pd audio" do
+    mock_audio = mock
+    mock_audio.expects(:start)
+    JRPD::Audio.expects(:new).returns(mock_audio)
+    jc = JRPDConnector.new
+    jc.start_audio
+  end
+
+  it "stops Pd audio" do
+    mock_audio = mock
+    mock_audio.expects(:stop)
+    JRPD::Audio.expects(:new).returns(mock_audio)
+    jc = JRPDConnector.new
+    jc.stop_audio
   end
 
 end
